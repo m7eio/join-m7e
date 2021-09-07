6d78fc1caeff3deef237ace58bd8e3f4709e483d
GoDotDotDot <chukui0202@gmail.com>
2021-09-07 03:46:16 +0000

    feat: add date

--- pages/api/form.ts
+++ pages/api/form.ts
@@ -1,31 +1,39 @@
-
 let jwtCache = '';
 
 export default async function handler(req, res) {
   const { method, body } = req;
 
   const submit = async (jwt) => {
-    return fetch('https://form.nft4metaverse.io/join-m7e/submission', {
-      method: 'POST',
-      headers: {
-        'Content-Type': 'application/json',
-        'x-jwt-token': jwt,
-      },
-      body: JSON.stringify({
-        data: body,
-      }),
-    });
+    try {
+      const jsonData = JSON.parse(body);
+      return fetch('https://form.nft4metaverse.io/join-m7e/submission', {
+        method: 'POST',
+        headers: {
+          'Content-Type': 'application/json',
+          'x-jwt-token': jwt,
+        },
+        body: JSON.stringify({
+          data: {
+            ...jsonData,
+            date: new Date(),
+          },
+        }),
+      });
+    } catch (err) {
+      console.log('form parse error');
+    }
   };
 
   if (method === 'POST') {
     const result = await submit(
-      jwtCache || 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJ1c2VyIjp7Il9pZCI6IjYxMzYxMGNmZDM5NDdmM2ZiNDIwMjNlMiJ9LCJmb3JtIjp7Il9pZCI6IjYxMzYwZjljYjM1YTY5NDAxMGRiZWIxMCJ9LCJpYXQiOjE2MzA5MzM0MDUsImV4cCI6MTYzMDk0NzgwNX0.TWqyuiYHXZ79CKrQNRE0hjAfJaCkHRa5TQsnYdc6T19',
+      jwtCache ||
+        'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJ1c2VyIjp7Il9pZCI6IjYxMzYxMGNmZDM5NDdmM2ZiNDIwMjNlMiJ9LCJmb3JtIjp7Il9pZCI6IjYxMzYwZjljYjM1YTY5NDAxMGRiZWIxMCJ9LCJpYXQiOjE2MzA5MzM0MDUsImV4cCI6MTYzMDk0NzgwNX0.TWqyuiYHXZ79CKrQNRE0hjAfJaCkHRa5TQsnYdc6T19',
     );
 
     const { status } = result;
-    console.log('status', status)
+    console.log('status', status);
     if (status < 200 || status >= 300) {
-      console.log('re login')
+      console.log('re login');
       const { headers } = await fetch('https://form.nft4metaverse.io/user/login', {
         method: 'POST',
         headers: {

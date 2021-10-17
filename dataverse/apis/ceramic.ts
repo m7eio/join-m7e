031e38056f480c680bd1714c12f3f82c05440e5e
QibingLee <qblee.zju@gmail.com>
2021-10-17 21:28:13 +0800

    check the ceramicApi version

--- dataverse/apis/ceramic.ts
+++ dataverse/apis/ceramic.ts
@@ -1,7 +1,10 @@
 import type { CeramicApi } from '@ceramicnetwork/common';
 import type { IDX } from '@ceramicstudio/idx';
 import { apis, BasicProfile } from 'dataverse-sdk';
-import {createThreeIdFromManager, authenticate } from './threeId'
+import { EthereumAuthProvider } from '@3id/connect';
+import { Manager } from './manager';
+
+import Ceramic from '@ceramicnetwork/http-client';
 
 let ceramic: CeramicApi;
 let idx: IDX;
@@ -21,7 +24,27 @@ export async function authenticateIDX(ethereumProvider: any, address: string): P
     ethereumProvider,
     address,
   });
-  await authenticate({ ceramic, didProvider });
+  await apis.threeId.authenticate({ ceramic, didProvider });
+}
+
+export async function createThreeIdFromManager({
+  ceramicApiHost,
+  ethereumProvider,
+  address,
+}: {
+  ceramicApiHost: string | undefined;
+  ethereumProvider: any;
+  address: string;
+}) {
+  const ceramic = new Ceramic(ceramicApiHost);
+  const ethereumAuthProvider = new EthereumAuthProvider(ethereumProvider, address);
+  
+  const manager = new Manager(ethereumAuthProvider, { ceramic });
+  const id = await manager.createAccount();
+
+  const didProvider = manager.didProvider(id);
+
+  return didProvider;
 }
 
 export async function initCollections(did?: string) {
